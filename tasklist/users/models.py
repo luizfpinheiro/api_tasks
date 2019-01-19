a7290ccb46d5693e610e26ba36ab496f556ef02f
luizfpinheiro <luizfelipenet@hotmail.com>
2019-01-19 02:41:12 -0200

    Adding validation in fields of user model

--- tasklist/users/models.py
+++ tasklist/users/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinValueValidator
 
 class User(models.Model):
+    USER_TYPES = (
+        ('admin','admin'),
+        ('manager','manager'),
+        ('default','default'),
+    )
+
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=30) ## EmailValidator
-    age = models.CharField(max_length=2) ## Min 18
-    user_type = models.CharField(max_length=7) ## Choices [admin, manager, default]
+    email = models.CharField(max_length=30, validators=[EmailValidator])
+    age = models.IntegerField(validators=[MinValueValidator(18)])
+    user_type = models.CharField(max_length=7, choices=USER_TYPES, default='default')

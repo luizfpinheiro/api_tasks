abf715646a36f8cfe2707969e45a60d6ae92e1ca
luizfpinheiro <luizfelipenet@hotmail.com>
2019-01-23 01:10:56 -0200

    Router with tasklist of an user by id

--- tasklist/tasklist/urls.py
+++ tasklist/tasklist/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from tasks.views import TaskViewSet
@@ -26,5 +25,8 @@ router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path(
+        'api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')
+        )
 ]
